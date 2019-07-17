6206788c8b4824bed1539eebbb7216543255b5de
ludonuno <ludovice.nuno@gmail.com>
2019-07-18 00:08:16 +0100

    Cors change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const sizeOf = require("object-sizeof")
-const cors = require('cors')
+//const cors = require('cors')
 const api = require("./api/api.js")
 
 const errorList = {
@@ -57,14 +57,19 @@ const routeList = {
 const app = express()
 const port = process.env.PORT || 3000
 
-const corsOptions = {
-	"origin": "*",
-	"methods": "GET,PUT,POST,DELETE",
-}
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors(corsOptions))
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-HEaders', '*')
+	if(req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE')
+		return res.status(200).json({});
+	}
+	next();
+})
+
+//app.use(cors(corsOptions))
 
 app.get("/API/:tabela", (req, res, next) => { //Search
 	switch (req.params.tabela) {
